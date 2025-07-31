81a45305b968100950ff52790bd451e9706c2eaa
sardor2009css <sardortoshtemirov621@gmail.com>
2025-07-31 15:19:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let name1 = prompt("Ismingizni kiriting")
+console.log(name1);
+let surname = prompt("Familyangizni kiriting")
+console.log(surname);
+let age = prompt("yoshngizni kiriting")
+console.log(age);
+let work = prompt("sohangizni yzoing")
+console.log(work);
+
+alert("Malumotingiz uchun rahmat")
+
